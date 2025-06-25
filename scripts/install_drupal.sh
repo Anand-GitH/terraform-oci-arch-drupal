618676bdb97c76b4ed841d93852300f317d26b02
Anand <anand6@buffalo.edu>
2025-06-24 22:34:08 -0700

    Update install_drupal.sh

--- scripts/install_drupal.sh
+++ scripts/install_drupal.sh
@@ -24,6 +24,10 @@ if [[ $is_quickstart == "true" ]]; then
   #install drush to install drupal thru CLI 
   php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
   php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+  echo "Executing user: $(whoami)"
+  echo "PATH before: $PATH"
+  export PATH=/usr/local/bin:$PATH
+  echo "PATH after: $PATH"
   composer config --no-plugins allow-plugins.drupal/core-composer-scaffold true
   composer require drupal/core-recommended drupal/core-composer-scaffold drush/drush --no-interaction
   vendor/bin/drush site:install standard --db-url='mysql://${drupal_name}:$encoded_drupal_password@${mds_ip}/${drupal_schema}' --site-name='${drupal_site}' --account-name='${drupal_account_name}' --account-pass='${drupal_account_password}' --yes
